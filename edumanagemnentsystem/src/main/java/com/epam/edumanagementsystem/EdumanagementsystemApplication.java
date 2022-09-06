ca869fb56ee05754d2630cb9c9e4875169a781bd
Gohar Minasyan <goharminasyan80@gmail.com>
2022-09-06 12:35:03 +0400

    popup

---
+++ edumanagemnentsystem/src/main/java/com/epam/edumanagementsystem/EdumanagementsystemApplication.java
@@ -0,0 +1,13 @@
+package com.epam.edumanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdumanagementsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdumanagementsystemApplication.class, args);
+	}
+
+}
